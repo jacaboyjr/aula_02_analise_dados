a7c4adeec7d0d23e02234ce7c236a549fa449d07
wjunior <junior.wander@gmail.com>
2025-07-15 22:17:14 -0300

    adicionado terceiro exercicio analise de dados

--- exercicios.py
+++ exercicios.py
@@ -10,6 +10,12 @@ print(f"A divisão inteira de {num1} por {nun2} é: {num1 // nun2}.")
 
 # escreva um programa que calcule a area do circulo recebendo o raio como entrada
 
-raio = float(input("Digite o raio do círculo: "))
+'''raio = float(input("Digite o raio do círculo: "))
 area_do_circulo = math.pi * (raio ** 2)
 print(f"A área do círculo com raio de {raio} é: {area_do_circulo:.2f}.")
+'''
+# Faça um programa que peça para o usuário digitar uma data no formado dd/mm/aaaa e imprima o dia, o mês e o ano separados.
+
+data = input("Digite uma data no formato dd/mm/aaaa: ")
+dia, mes, ano = data.split('/')
+print(f"O dia é: {dia}, o mês é: {mes} e o ano é: {ano}.")
