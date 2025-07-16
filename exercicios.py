2d637199f98bf04f493121acd19c0563ef7ea2c3
wjunior <junior.wander@gmail.com>
2025-07-15 22:05:03 -0300

    Resolvendo primeiro exercicio aula 02 analise de dados

---
+++ exercicios.py
@@ -0,0 +1,6 @@
+# desenvolva um programa que peça dois numeros inteiros e imprima a divisão inteira do primeiro pelo segundo
+
+num1 = int (input("Digite o primeiro número inteiro: "))
+nun2 = int(input("Digite o segundo número inteiro: "))
+
+print(f"A divisão inteira de {num1} por {nun2} é: {num1 // nun2}.")
